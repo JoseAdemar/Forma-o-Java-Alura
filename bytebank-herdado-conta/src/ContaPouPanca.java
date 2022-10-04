8808dd812c2eabf8875af14118f9d9145fb096fc
jose-junior <joseademarti@gamil.com>
2022-10-03 21:30:34 -0300

    Estudando polimorfismo e herança

---
+++ bytebank-herdado-conta/src/ContaPouPanca.java
@@ -0,0 +1,8 @@
+
+public class ContaPouPanca extends Conta {
+
+	public ContaPouPanca(int agencia, int numero) {
+		super(agencia, numero);
+	}
+
+}
---
+++ bytebank-herdado-conta/src/TesteContas.java
@@ -0,0 +1,22 @@
+
+public class TesteContas {
+
+	public static void main(String[] args) {
+		
+		ContaPouPanca cp = new ContaPouPanca(222, 222);
+		cp.deposita(200);
+		
+		
+		ContaCorrente cc = new ContaCorrente(111, 111);
+		cc.deposita(100);
+		cc.transfere(10, cp);
+		cc.saca(10);
+		
+		System.out.println("CC " + cc.getSaldo());
+		System.out.println("CP " + cp.getSaldo());
+		
+		
+		
+		
+	}
+}
