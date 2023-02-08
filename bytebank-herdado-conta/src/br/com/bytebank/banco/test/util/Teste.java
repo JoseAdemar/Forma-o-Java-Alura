ef9ff0feba74243721163db0487bc5f5e8fbb411
learnportuguesefrombrazil <portugueseteacherdf@gmail.com>
2023-02-08 14:04:53 -0300

    Estudando Lambas do Java

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -44,29 +44,13 @@ public class Teste {
         lista.add(cc3);
         lista.add(cc4);
 
-        lista.sort(new Comparator<Conta>() { //classe anonima
+        lista.sort(Comparator.comparingInt(Conta::getNumero));
 
-                       @Override
-                       public int compare(Conta c1, Conta c2) {
+        lista.forEach((conta) -> System.out.println(conta + ", "  + conta.getTitular().getNome()));
 
-                           return Integer.compare(c1.getNumero(), c2.getNumero());
-                       }
-                   }
-        );
-
-        Comparator<Conta> comp = new Comparator<Conta>() {
-
-            @Override
-            public int compare(Conta c1, Conta c2) {
-                String nomeC1 = c1.getTitular().getNome();
-                String nomeC2 = c2.getTitular().getNome();
-                return nomeC1.compareTo(nomeC2);
-            }
-        };
-
-        for (Conta conta : lista) {
-            System.out.println(conta + ", " + conta.getTitular().getNome());
-        }
+//        for (Conta conta : lista) {
+//            System.out.println(conta + ", " + conta.getTitular().getNome());
+//        }
     }
 }
 
